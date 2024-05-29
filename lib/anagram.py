5d667569a179f7f1da284cd0becd7cd550836840
Chantale <shantelenos99@gmail.com>
2024-05-29 09:29:32 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -3,9 +3,12 @@ class Anagram:
     def __init__(self, word):
         self.word = word
 
-        def match (self, words):
-            anagrams = []
-            for word in words:
-                if sorted(self.word) == sorted(word):
-                    anagrams.append(word)
-                    return anagrams
+
+    def match(self, words):
+        return [word for word in words if sorted(word) == sorted(self.word) and word!= self.word]
+    
+
+
+   
+
+     
